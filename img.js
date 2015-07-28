830dcd254dcc7eee0dd143994c0920efb877a0de
leonardo rolon <kabeto_13@hotmail.com>
2015-07-28 16:50:37 -0500

    barca

---
+++ img.js
@@ -0,0 +1,27 @@
+function img6() {
+document.getElementById('img').src='./img/img.jpg';
+}
+function img1() {
+document.getElementById('img').src='./img/img1.jpg';
+}
+
+
+function img2() {
+document.getElementById('img').src='./img/img2.jpg';
+}
+
+
+function img3() {
+document.getElementById('img').src='./img/img3.jpg';
+}
+
+
+function img4() {
+document.getElementById('img').src='./img/img4.jpg';
+}
+function img5 () {
+document.getElementById("img").src="./img/img5.jpg"}
+
+
+function volver () {
+document.getElementById("img").src="./img/img5.jpg"}
